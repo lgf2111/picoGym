ca5f6b3e04eddada284e582ef9851d5e90ec9431
lgf2111 <lgf2111@gmail.com>
2022-05-11 17:41:30 +0800

    reset

--- Forensics/information/solve.py
+++
@@ -1,4 +0,0 @@
-import base64
-
-FLAG = 'cGljb0NURnt0aGVfbTN0YWRhdGFfMXNfbW9kaWZpZWR9'
-print(base64.b64decode(FLAG))
--- General Skills/Obedient Cat/solve.py
+++
@@ -1,4 +0,0 @@
-import codecs
-
-FLAG = "cvpbPGS{arkg_gvzr_V'yy_gel_2_ebhaqf_bs_ebg13_nSkgmDJE}"
-print(codecs.decode(FLAG, 'rot_13'))
